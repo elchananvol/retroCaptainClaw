8399df8b2e4fa3fa632ce6084fe54619f7b5701a
Elchanan Vol <elchanan.vol@gmail.com>
2021-12-26 20:10:46 +0200

    end init

---
+++ src/pepse/PepseGameManager.java
@@ -0,0 +1,14 @@
+package pepse;
+
+import danogl.GameManager;
+
+public class PepseGameManager extends GameManager {
+    PepseGameManager() {
+    }
+
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+
+}
+
--- src/pepse/pepseGameManager.java
+++
@@ -1,7 +0,0 @@
-package pepse;
-
-public class pepseGameManager {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
