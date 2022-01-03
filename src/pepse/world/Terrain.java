ad334b92a35abc653ae8f5a56c3a16d30815f56d
Elchanan Vol <elchanan.vol@gmail.com>
2022-01-03 13:03:26 +0200

    change function so that create blocks of terrain in once

--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -30,20 +30,23 @@ public class Terrain {
     public float groundHeightAt(float x) {
 //        x =Block.round(x);
         x= (int) Math.floor( x / (Block.SIZE*3)) * Block.SIZE*3;
-        return windowDimensions.y()-  Block.round((float) noiseGenerator.noise((double) x) * Block.SIZE * 5 + windowDimensions.y() / 4);
+        return windowDimensions.y()-  Block.round((float) noiseGenerator.noise((double) x) * Block.SIZE * 10 + windowDimensions.y() / 4);
     }
 
     public void createInRange(int minX, int maxX) {
         assert maxX >= minX;
-        minX =  Block.round(minX);
-        maxX = Block.round(maxX);
-        for (int i = minX; i <= maxX; i += Block.SIZE ) {
+        minX = (int) Math.floor( (float)minX / (Block.SIZE*3)) * Block.SIZE*3;
+        maxX = (int) Math.floor((float) maxX / (Block.SIZE*3)) * Block.SIZE*3;
+        for (int i = minX; i <= maxX; i += Block.SIZE*3 ) {
+            GameObject block = new Block(new Vector2(i,  groundHeightAt(i)), new Vector2(Block.SIZE*3, windowDimensions.y()-groundHeightAt(i)), new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+            gameObjects.addGameObject(block, groundLayer);
+
 //            for (int j = i; j <= i + Block.SIZE * 2; j += Block.SIZE) {
-                for (int hight = (int)windowDimensions.y() - Block.SIZE; hight >= (int)groundHeightAt(i); hight -= Block.SIZE) {
-                    Vector2 location = new Vector2(i,  hight);
-                    GameObject block = new Block(location, new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
-                    gameObjects.addGameObject(block, groundLayer);
-                }
+//                for (int hight = (int)windowDimensions.y() - Block.SIZE; hight >= (int)groundHeightAt(i); hight -= Block.SIZE) {
+//                    Vector2 location = new Vector2(i,  hight);
+//                    GameObject block = new Block(location, new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR)));
+//                    gameObjects.addGameObject(block, groundLayer);
+//                }
 //            }
 
 
